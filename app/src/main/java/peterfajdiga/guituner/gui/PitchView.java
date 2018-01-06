324526e0f6ad3fa6f6b877aca48ce720c588480a
Peter Fajdiga <peter.fajdiga@gmail.com>
2018-01-06 17:41:50 +0100

    selecting frequencies

--- app/src/main/java/peterfajdiga/guituner/gui/PitchView.java
+++ app/src/main/java/peterfajdiga/guituner/gui/PitchView.java
@@ -3,11 +3,13 @@ package peterfajdiga.guituner.gui;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewParent;
 import android.widget.ScrollView;
@@ -24,9 +26,11 @@ public class PitchView extends View {
     private static final float EDGE_TONE_OFFSET_Y = 46.0f;
     private static final float LINE_TEXT_SPACING = 6.0f;
     private static final float TONE_LINE_LENGTH = 46.0f;
+    private static final float SELECTION_WIDTH = 48.0f;
 
     private static final Paint paint_tone = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private static final Paint paint_freq  = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private static final Paint paint_freq = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private static final Paint paint_selection = new Paint();
 
     private final android.graphics.Rect textBounds = new Rect();
 
@@ -34,6 +38,7 @@ public class PitchView extends View {
     private int width, height;
 
     private double detectedFrequency = 0.0;
+    private Tone selectedTone = null;
 
     public PitchView(Context context) {
         super(context);
@@ -56,6 +61,15 @@ public class PitchView extends View {
     }
 
 
+    private ScrollView getParentScrollView() {
+        final ViewParent parent = getParent();
+        if (parent instanceof ScrollView) {
+            return (ScrollView) parent;
+        } else {
+            throw new RuntimeException("PitchView not in a ScrollView");
+        }
+    }
+
     public void setFrequency(final double frequency) {
         if (frequency <= 0.0) {
             return;
@@ -66,21 +80,43 @@ public class PitchView extends View {
     }
 
     private void focusOnFrequency(final double frequency) {
-        final ViewParent parent = getParent();
-        if (parent instanceof ScrollView) {
-            final ScrollView scrollView = (ScrollView)parent;
-            final int screenHeightHalf = scrollView.getHeight() / 2;
-            int y = (int)getFrequencyY(frequency) - screenHeightHalf;
-            if (y < screenHeightHalf) {
-                y = screenHeightHalf;
-            } else if (y > height - screenHeightHalf) {
-                y = height - screenHeightHalf;
-            }
-            scrollView.smoothScrollTo(0, y);
-        } else {
-            throw new RuntimeException("PitchView not in a ScrollView");
+        final ScrollView parent = getParentScrollView();
+        final int screenHeightHalf = parent.getHeight() / 2;
+        int y = (int)getFrequencyY(frequency) - screenHeightHalf;
+        if (y < screenHeightHalf) {
+            y = screenHeightHalf;
+        } else if (y > height - screenHeightHalf) {
+            y = height - screenHeightHalf;
         }
+        parent.smoothScrollTo(0, y);
     }
+
+
+
+    // control
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        final ScrollView parent = getParentScrollView();
+        final int y = parent.getScrollY() + parent.getHeight() / 2;
+        final double frequency = getFrequencyFromY(y);
+
+        double minDiff = Double.MAX_VALUE;
+        for (Tone tone : tones) {
+            final double diff = Math.abs(tone.frequency - frequency);
+            if (diff < minDiff) {
+                minDiff = diff;
+                selectedTone = tone;
+            }
+        }
+        invalidate();
+        return true;
+    }
+
+    /*@Override
+    public boolean performClick() {
+        System.err.println("asdf");
+        return true;
+    }*/
 
 
 
@@ -97,6 +133,8 @@ public class PitchView extends View {
         paint_freq.setTextAlign(Paint.Align.RIGHT);
         paint_freq.setColor(r.getColor(android.R.color.tertiary_text_light));
         paint_freq.setStrokeWidth(dp);
+
+        paint_selection.setColor(Color.LTGRAY);
     }
 
     @Override
@@ -112,6 +150,35 @@ public class PitchView extends View {
     protected void onDraw(Canvas canvas) {
         width  = canvas.getWidth();   // probably unnecessary
         height = canvas.getHeight();  // probably unnecessary
+
+
+        // draw tones
+        final float toneX = width * TONE_OFFSET_X_RATIO;
+        for (Tone tone : tones) {
+            final float toneY = getFrequencyY(tone.frequency);
+
+            if (tone == selectedTone) {
+                final float selectionHalfWidth = SELECTION_WIDTH * dp / 2.0f;
+                canvas.drawRect(0, toneY - selectionHalfWidth, width, toneY + selectionHalfWidth, paint_selection);
+            }
+
+            paint_tone.getTextBounds(tone.name, 0, tone.name.length(), textBounds);
+
+            canvas.drawText(tone.name, toneX, getCenteredY(toneY), paint_tone);
+
+            final float toneLineLeftStartX = getTextLeft(toneX, paint_tone) - LINE_TEXT_SPACING * dp;
+            final float toneLineRightStartX = getTextRight(toneX, paint_tone) + LINE_TEXT_SPACING * dp;
+            canvas.drawLine(
+                    toneLineLeftStartX - TONE_LINE_LENGTH, toneY,
+                    toneLineLeftStartX, toneY,
+                    paint_tone
+            );
+            canvas.drawLine(
+                    toneLineRightStartX, toneY,
+                    toneLineRightStartX + TONE_LINE_LENGTH, toneY,
+                    paint_tone
+            );
+        }
 
 
         // draw detected frequency
@@ -133,30 +200,6 @@ public class PitchView extends View {
                     getTextRight(freqX, paint_freq) + LINE_TEXT_SPACING * dp, freqY,
                     width, freqY,
                     paint_freq
-            );
-        }
-
-
-        // draw tones
-        final float toneX = width * TONE_OFFSET_X_RATIO;
-        for (Tone tone : tones) {
-            final float toneY = getFrequencyY(tone.frequency);
-
-            paint_tone.getTextBounds(tone.name, 0, tone.name.length(), textBounds);
-
-            canvas.drawText(tone.name, toneX, getCenteredY(toneY), paint_tone);
-
-            final float toneLineLeftStartX = getTextLeft(toneX, paint_tone) - LINE_TEXT_SPACING * dp;
-            final float toneLineRightStartX = getTextRight(toneX, paint_tone) + LINE_TEXT_SPACING * dp;
-            canvas.drawLine(
-                    toneLineLeftStartX - TONE_LINE_LENGTH, toneY,
-                    toneLineLeftStartX, toneY,
-                    paint_tone
-            );
-            canvas.drawLine(
-                    toneLineRightStartX, toneY,
-                    toneLineRightStartX + TONE_LINE_LENGTH, toneY,
-                    paint_tone
             );
         }
     }
@@ -198,6 +241,18 @@ public class PitchView extends View {
 
         final double freq01 = (logFreq - minLogFreq) / (maxLogFreq - minLogFreq);
         return minY + (float)freq01 * (maxY - minY);
+    }
+
+    private double getFrequencyFromY(final float y) {
+        final float minY = EDGE_TONE_OFFSET_Y * dp;
+        final float maxY = height - EDGE_TONE_OFFSET_Y * dp;
+
+        final double minLogFreq = Math.log(tones[0].frequency);
+        final double maxLogFreq = Math.log(tones[tones.length-1].frequency);
+
+        final double freq01 = (y - minY) / (maxY - minY);
+        final double logFreq = (freq01 * (maxLogFreq - minLogFreq)) + minLogFreq;
+        return Math.exp(logFreq);
     }
 
 
