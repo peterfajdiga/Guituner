4521e14be571a2af31a968cdfc9d4e3d744b081d
Peter Fajdiga <peter.fajdiga@gmail.com>
2020-05-20 20:14:22 +0200

    using primary and secondary text colors for tuning radio buttons

--- app/src/main/java/peterfajdiga/guituner/app/tuning/CustomTuning.java
+++ app/src/main/java/peterfajdiga/guituner/app/tuning/CustomTuning.java
@@ -1,13 +1,15 @@
 package peterfajdiga.guituner.app.tuning;
 
+import android.content.Context;
+
 import androidx.annotation.NonNull;
 
 public class CustomTuning extends Tuning {
-    public CustomTuning(@NonNull final String name) {
-        this(name, "");
+    public CustomTuning(@NonNull final String name, @NonNull final Context context) {
+        this(name, "", context);
     }
 
-    public CustomTuning(@NonNull final String name, @NonNull final String tonesString) {
-        super(name, tonesString);
+    public CustomTuning(@NonNull final String name, @NonNull final String tonesString, @NonNull final Context context) {
+        super(name, tonesString, context);
     }
 }
--- app/src/main/java/peterfajdiga/guituner/app/tuning/Tuning.java
+++ app/src/main/java/peterfajdiga/guituner/app/tuning/Tuning.java
@@ -1,8 +1,14 @@
 package peterfajdiga.guituner.app.tuning;
 
+import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Color;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 
 import peterfajdiga.guituner.R;
 import peterfajdiga.guituner.gui.views.ItemedRadioGroup;
@@ -11,21 +17,33 @@ public class Tuning implements ItemedRadioGroup.Item {
     public final String name;
     public final String tonesString;
 
-    public Tuning(@NonNull final String name, @NonNull final String tonesString) {
+    private final int secondaryColor;
+
+    public Tuning(@NonNull final String name, @NonNull final String tonesString, @NonNull final Context context) {
         this.name = name;
         this.tonesString = tonesString;
+
+        this.secondaryColor = ContextCompat.getColor(context, R.color.secondary_text);
     }
 
     @Override
     public CharSequence getButtonText() {
-        return name + "\n" + tonesString;
+        final SpannableString str = new SpannableString(name + "\n" + tonesString);
+        str.setSpan(
+                new ForegroundColorSpan(secondaryColor),
+                name.length() + 1,
+                str.length(),
+                Spannable.SPAN_INCLUSIVE_EXCLUSIVE
+        );
+        return str;
     }
 
     @NonNull
-    public static Tuning[] getBuiltInTunings(@NonNull final Resources resources) {
+    public static Tuning[] getBuiltInTunings(@NonNull final Context context) {
+        final Resources resources = context.getResources();
         return new Tuning[] {
-                new Tuning(resources.getString(R.string.tuning_standard), "E2 A2 D3 G3 B3 E4"),
-                new Tuning(resources.getString(R.string.tuning_drop_d), "D2 A2 D3 G3 B3 E4"),
+                new Tuning(resources.getString(R.string.tuning_standard), "E2 A2 D3 G3 B3 E4", context),
+                new Tuning(resources.getString(R.string.tuning_drop_d), "D2 A2 D3 G3 B3 E4", context),
         };
     }
 }
