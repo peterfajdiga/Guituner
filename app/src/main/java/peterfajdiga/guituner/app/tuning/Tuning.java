0cccc625b52b5250e54703b557512acde6557e5b
Peter Fajdiga <peter.fajdiga@gmail.com>
2020-06-03 18:44:28 +0200

    add Violin standard tuning

--- app/src/main/java/peterfajdiga/guituner/app/tuning/Tuning.java
+++ app/src/main/java/peterfajdiga/guituner/app/tuning/Tuning.java
@@ -75,6 +75,7 @@ public class Tuning implements ItemedRadioGroup.Item {
                 new Tuning(resources.getString(R.string.tuning_major_thirds), "G2# C3 E3 G3# C4 E4", style),
                 new Tuning(resources.getString(R.string.tuning_all_fifths), "C2 G2 D3 A3 E4 B4", style),
 
+                new Tuning(resources.getString(R.string.tuning_violin_standard), "G3 D4 A4 E4", style),
                 new Tuning(resources.getString(R.string.tuning_ukulele_soprano), "G4 C4 E4 A4", style),
         };
     }
